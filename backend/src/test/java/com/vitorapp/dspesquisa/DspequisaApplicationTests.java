c0703ca7d8e33187c7f89d0e12e3bb8c70e5d92e
Vitor Lucas <vitorcrispim@yahoo.com.br>
2020-09-16 20:18:40 -0300

    Project created

---
+++ backend/src/test/java/com/vitorapp/dspesquisa/DspequisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorapp.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspequisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
